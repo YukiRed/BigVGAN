c8b103f0c47a03ea9d56cc1c85748d5d6cc57f71
Desmond <16297519+YukiRed@users.noreply.github.com>
2024-11-16 10:56:34 +0800

    Update bigvgan.py for f5-tts

--- bigvgan.py
+++ bigvgan.py
@@ -5,6 +5,10 @@
 #   LICENSE is in incl_licenses directory.
 
 import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import json
 from pathlib import Path
 from typing import Optional, Union, Dict
